ca26cb8fca079e0a9109252d70197e823c2a285a
Zippstorm <29355721+Zippstorm-g5@users.noreply.github.com>
2024-02-24 17:18:14 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -25,17 +25,17 @@ username=$(jq -r '.inbounds[0].users[0].username' /usr/local/etc/sing-box/config
 countrycode=${username: -5:2}
 
 
-RECORD_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/${CF_Zone_ID}/dns_records?name=${countrycode}.zippstorm.com" \
+RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${CF_Zone_ID}/dns_records?name=${countrycode}.zippstorm.com" \
      -H "Authorization: Bearer ${CF_Token}" \
      -H "Content-Type: application/json" | jq -r '.result[0].id')
 
 
 if [ "$ip_type" == "A" ]; then
     # 如果IP类型是A，获取IPv4地址
-    address=$(curl -4 ip.sb)
+    address=$(curl -s -4 ip.sb)
 elif [ "$ip_type" == "AAAA" ]; then
     # 如果IP类型是AAAA，获取IPv6地址
-    address=$(curl -6 ip.sb)
+    address=$(curl -s -6 ip.sb)
 else
     echo "Unknown IP type: $ip_type"
     exit 1
@@ -44,12 +44,12 @@ echo $countrycode
 echo $address
 echo $RECORD_ID
 if [ -z "$RECORD_ID" ] || [ "$RECORD_ID" = "null" ]; then
-     curl -X POST "https://api.cloudflare.com/client/v4/zones/${CF_Zone_ID}/dns_records" \
+     curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${CF_Zone_ID}/dns_records" \
           -H "Authorization: Bearer ${CF_Token}" \
           -H "Content-Type: application/json" \
           --data "{\"type\":\"${ip_type}\",\"name\":\"${countrycode}.zippstorm.com\",\"content\":\"${address}\",\"ttl\":60,\"proxied\":false}"
 else
-     curl -X PUT "https://api.cloudflare.com/client/v4/zones/${CF_Zone_ID}/dns_records/${RECORD_ID}" \
+     curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${CF_Zone_ID}/dns_records/${RECORD_ID}" \
           -H "Authorization: Bearer ${CF_Token}" \
           -H "Content-Type:application/json" \
           --data "{\"type\":\"${ip_type}\",\"name\":\"${countrycode}.zippstorm.com\",\"content\":\"${address}\",\"ttl\":60,\"proxied\":false}"
